e5a48ed223bf83467bf60e6d97e48a7182e8c818
Patrick Kelly <ox6185ys@go.minneapolis.edu>
2017-03-01 12:27:42 -0600

    Site Update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var exchangeRates = {'EUR' : .94, 'JPY' : 112.86, 'GBP' : .79716, 'USD' : 1};
+var exchangeRates = {'EUR' : .94, 'JPY' : 112.86, 'GBp' : .79716, 'USD' : 1};
 //'GBP' : .797157, 'Rupee' : 66.69,
 router.get('/', function(req, res){
 
@@ -13,6 +13,12 @@ router.get('/convert', function(req, res){
   var convertTo = req.query.to_currency;
   var rate = exchangeRates[convertTo];
   result = dollars * rate;
+
+  //console.log(dollars);
+  console.log(rate);
+  console.log(dollars);
+  console.log(convertTo);
+
   res.render('results', { dollars : dollars, result: result, currency: convertTo})
 
 });
